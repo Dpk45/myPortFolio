2e60f1379241d01e3035ac7f6ddb5a7ea1c63228
dpk45 <dpkchauhan1994@gmail.com>
2017-07-09 23:38:51 +0530

    Basic Home page creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing, appRoutingProviders } from "./app.route"
 
 import { AppComponent } from './app.component';
+import {HomeComponent}  from  '../app/component/home/home.component';
+import {AboutComponent}  from  '../app/component/about/about.component';
+import {SkillsComponent}  from  '../app/component/skills/skills.component';
+import {EducationComponent}  from  '../app/component/education/education.component';
+import {ContactComponent}  from  '../app/component/contact/contact.component';
+import {NavbarComponent}  from  '../app/component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    EducationComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     FormsModule,
+    HttpModule,routing
   ],
   providers: [],
   bootstrap: [AppComponent]
